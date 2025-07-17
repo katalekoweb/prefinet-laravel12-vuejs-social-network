8572bbdd319e80a3329e4277b403f19d071b4845
Kataleko <katalekoweb@gmail.com>
2025-07-17 21:30:31 +0100

    tag controller

--- app/Http/Controllers/Admin/TagController.php
+++ app/Http/Controllers/Admin/TagController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\TagRequest;
+use App\Models\Tag;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class TagController extends Controller
 {
@@ -12,7 +15,11 @@ class TagController extends Controller
      */
     public function index()
     {
-        //
+        $tags = Tag::query()
+        // @TODO filters
+        ->paginate(10);
+
+        return Inertia::render("Admin/Tags/Index", compact('tags'));
     }
 
     /**
@@ -20,46 +27,50 @@ class TagController extends Controller
      */
     public function create()
     {
-        //
+        $tag = new Tag;
+        return Inertia::render("Admin/tags/Form", compact('tag'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(TagRequest $request)
     {
-        //
+        $tag = Tag::query()->create($request->validated());
+        return redirect(route('admin.tags.index'))->with('success', __('tag created'));
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Tag $tag)
     {
-        //
+        return Inertia::render("Admin/tags/Form", compact('tag'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Tag $tag)
     {
-        //
+        return Inertia::render("Admin/tags/Form", compact('tag'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(TagRequest $request, Tag $tag)
     {
-        //
+        $tag->update($request->validated());
+        return redirect(route('admin.tags.index'))->with('success', __('tag updated'));
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Tag $tag)
     {
-        //
+        $tag->delete();
+        return redirect(route('admin.tags.index'))->with('success', __('tag deleted'));
     }
 }
